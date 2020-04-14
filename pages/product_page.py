f716ae67bcd4ebd76e8be7187b35fb705f53380d
vanapagan <vanapagan@list.ru>
2020-04-14 17:22:44 +0300

    add functions should_be_message_about_add_to_cart and should_be_message_about_price for check correct book's name and correct price in cart

--- pages/product_page.py
+++ pages/product_page.py
@@ -8,3 +8,13 @@ class ProductPage(BasePage):
     def add_to_cart(self):
         button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
         button.click()
+
+    def should_be_message_about_add_to_cart(self):
+        item_name = self.browser.find_element(*ProductPageLocators.ITEM_NAME)
+        item_name_in_message = self.browser.find_element(*ProductPageLocators.MESSAGE_ABOUT_ITEM)
+        assert item_name.text in item_name_in_message.text, f"expected item_name is '{item_name.text}', but u got'{item_name_in_message.text}' "
+
+    def should_be_message_about_price(self):
+        price = self.browser.find_element(*ProductPageLocators.PRICE)
+        price_in_cart = self.browser.find_element(*ProductPageLocators.MESSAGE_PRICE)
+        assert price.text in price_in_cart.text, f"expected item_cost is '{price.text}', but u got in card'{price_in_cart.text}' "
--- test_product_page.py
+++ test_product_page.py
@@ -10,4 +10,8 @@ def test_guest_can_add_product_to_basket(browser):
     page.open()                         # открываем страницу
     page.add_to_cart()                  # выполняем метод страницы
     # login_page = page.go_to_login_page()
-    time.sleep(5)
+    #time.sleep(2)
+    page.solve_quiz_and_get_code()
+    #time.sleep(2)
+    page.should_be_message_about_add_to_cart()
+    page.should_be_message_about_price()
