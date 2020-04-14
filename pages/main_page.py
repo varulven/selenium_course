9f07deb97c65e6fde41151902ff21ed95b5c4ad2
vanapagan <vanapagan@list.ru>
2020-04-14 15:13:47 +0300

    add LoginPage init in test, remove return (second way from stepik 4.2.9)

--- pages/main_page.py
+++ pages/main_page.py
@@ -9,7 +9,7 @@ class MainPage(BasePage):
         # login_link = self.find_element_by_css_selector("#login_link")
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url)
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
