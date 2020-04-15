1b07e2f7a57ee851e3b13bb894788a1d205c79fc
vanapagan <vanapagan@list.ru>
2020-04-15 17:36:50 +0300

    изменения для перехода к логину с любой страницы

--- pages/locators.py
+++ pages/locators.py
@@ -4,9 +4,11 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
+
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+
 
 class ProductPageLocators():
     ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn-add-to-basket")
@@ -16,3 +18,8 @@ class ProductPageLocators():
     PRICE = (By.CSS_SELECTOR, ".product_main .price_color")
     MESSAGE_PRICE = (By.CSS_SELECTOR, ".alert-info .alertinner strong")
     # FOR_TEST = (By.CSS_SELECTOR, ".instock")
+
+
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
--- test_product_page.py
+++ test_product_page.py
@@ -8,6 +8,20 @@ link2 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?pro
 link3 = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+
+
 def test_guest_can_add_product_to_basket(browser):
     # link1 = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser,
@@ -21,6 +35,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_message_about_price()
     page.text_in_message_about_add_to_cart()
     page.price_in_message_about_price()
+
 
 @pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
@@ -38,6 +53,7 @@ def test_guest_cant_see_success_message(browser):
                        link3)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.should_not_be_success_message()
+
 
 @pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
